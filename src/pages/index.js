8704d9289c540c797edbfee0f71c446d5953580b
Usama3414 <uriaz8012@gmail.com>
2023-05-20 09:44:59 -0700

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Layout from '@/components/Layout'
 import Image from 'next/image'
 import profpic from "../../public/images/profile/developer-pic-2.png";
+import AnimatedText from '@/components/AnimatedText';
 export default function Home() {
   return (
     <>
@@ -13,17 +14,17 @@ export default function Home() {
       <main className='flex items-center justify-center text-dark w-full min-h-screen '>
       <Layout  className='pt-0'>
        <div class = "flex items-center justify-center w-full">
-       <div   className='w-1/2'>
-        <Image src={profpic} alt ="UsamaRiaz"  className='h-full w-auto z-10 ' />
+       <div className='w-1/2'>
+        <Image src = {profpic} alt ="CodeBucks" className='h-50 w-auto ' />
        </div>
-       <div>
-        <h1>Turning Vision Into Reality With Code And Design</h1>
-        <p> 
+       <div className='w-1/2  flex flex-col items-center self-center '>
+        <AnimatedText text= "Turning Vision Into Reality With Code And Design" className='!text-8xl'/>
+      <p> 
         As a skilled full-stack developer, I am dedicated to turning ideas into innovative web applications. 
         my latest projects and articles, showcasing my expertise in Metaverse and web 
         development </p>
-       </div>
-       </div>
+      </div>
+      </div>
       </Layout>
       </main>
     </>
